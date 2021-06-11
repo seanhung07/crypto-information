db7bfc3a0ab6bbc6af64a4fb4b6fbb02f86c5291
Sean Hung <35648173+seanhung07@users.noreply.github.com>
2021-06-11 09:50:44 +0800

    test

--- src/components/News/Whale.jsx
+++ src/components/News/Whale.jsx
@@ -4,14 +4,14 @@ function Whale(){
 
     useEffect(()=>{
         async function fetchData(){
-            const request = await axios.get('https://api.whale-alert.io/feed.csv');
-            console.log(request)
+            const request = await axios.get('https://api.whale-alert.io/feed.csv?api_key=YDV2AMXZoLApRCNHvlQjKzF6I0h2SEzL');
+            // console.log(request)
         }
         fetchData()
     
     },[])
     return(
-        <div>Hello</div>
+        <div></div>
     )
 
 
