e4876d41c7f3ab6f3e277f9eef9b8f6cd95caaea
Sean Hung <35648173+seanhung07@users.noreply.github.com>
2021-06-21 23:30:16 +0800

    route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
-import {Route, BrowserRouter as Router, Switch} from 'react-router-dom'
+import { HashRouter,Route, BrowserRouter as Router, Switch} from 'react-router-dom'
 import Home from './Home'
 import DefiData from './components/Defi/DefiData'
 
 function App(){
   return(
     <Router>
-      <Switch>
-        <Route path="/crypto-information" exact component={Home}/>
-        <Route path="/crypto-information/defi" component={DefiData}/>
-      </Switch>
+        <Switch>
+          <Route exact path="/crypto-information"  component={Home}/>
+          <Route exact path="/crypto-information/defi" component={DefiData}/>
+        </Switch>
     </Router>
   )
 };
--- src/components/Defi/DefiData.jsx
+++ src/components/Defi/DefiData.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {ThemeProvider, createMuiTheme} from '@material-ui/core/styles'
 import { CssBaseline} from "@material-ui/core";
+import ReactApexChart from 'react-apexcharts'
 const theme = createMuiTheme({
     palette: {
       type: "dark"
@@ -11,10 +12,66 @@ const theme = createMuiTheme({
     }
   });
 function Defi(){
+  const series = [{
+    name: 'Marine Sprite',
+    data: [44, 55, 41, 37, 22, 43, 21]
+  }, {
+    name: 'Striking Calf',
+    data: [53, 32, 33, 52, 13, 43, 32]
+  }, {
+    name: 'Tank Picture',
+    data: [12, 17, 11, 9, 15, 11, 20]
+  }, {
+    name: 'Bucket Slope',
+    data: [9, 7, 5, 8, 6, 9, 4]
+  }, {
+    name: 'Reborn Kid',
+    data: [25, 12, 19, 32, 25, 24, 10]
+  }]
+  const options = {
+    chart: {
+      type: 'bar',
+      height: 350,
+      stacked: true,
+      stackType: '100%'
+    },
+    plotOptions: {
+      bar: {
+        horizontal: true,
+      },
+    },
+    stroke: {
+      width: 1,
+      colors: ['#fff']
+    },
+    title: {
+      text: '100% Stacked Bar'
+    },
+    xaxis: {
+      categories: [2008, 2009, 2010, 2011, 2012, 2013, 2014],
+    },
+    tooltip: {
+      y: {
+        formatter: function (val) {
+          return val + "K"
+        }
+      }
+    },
+    fill: {
+      opacity: 1
+    
+    },
+    legend: {
+      position: 'top',
+      horizontalAlign: 'left',
+      offsetX: 40
+    }
+  }
     return(
         <ThemeProvider theme={theme}>
             <CssBaseline />
-            <div>Defi</div>
+            <ReactApexChart options={options} series={series} type="bar" height={350} />
+    
         </ThemeProvider>
     )
 }
